4d6ee430b4aabfcf8731f03d6a62b8b914c74241
castelstack <okaforhenry01@gmail.com>
2024-05-27 16:36:46 -0700

    feat: logo dark mode svg and state added

--- components/atoms/Logo.tsx
+++ components/atoms/Logo.tsx
@@ -1,15 +1,19 @@
+'use client';
+
+import { useTheme } from 'next-themes';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
 export const Logo = () => {
+  const { theme } = useTheme();
   return (
     <Link href={'/'}>
       <Image
         width={500}
         height={50}
         objectFit='contain'
-        src={'/logo.svg'}
+        src={theme === 'light' ? '/logo.svg' : '/logoWhite.svg'}
         alt='logo'
         className='h-[50px] w-[50px]'
       />
